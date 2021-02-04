b67439fddc89f3affd393af55bd38c7097f7138a
Abderahmane Aoufi <abdouou7@gmail.com>
2021-02-04 22:06:18 +0100

    nothing happened

--- site/javascript/sign-up.js
+++ site/javascript/sign-up.js
@@ -1,85 +1,90 @@
-const emailT = $('#dataeml');
-const passwordT = $('#pass');
-const loginBtn = $('#loginBtn');
+const emailT = $("#dataeml");
+const passwordT = $("#pass");
+const loginBtn = $("#loginBtn");
 
-let email , password ; 
-$(document).ready(function(){
-    // alert("hellow ")
-    loginBtn.click(function(){
-        setVariables();
-        if(checkEmailValidation(email) == true ){
-           if(checkPasswordValidation(password) == true){
-               cleanUpWarning();
-               let data = {
-                   email,password
-               }
-               callBackEnd(data);
-            
-           }else{
-            showPasswordwarning("your password should be 8 chracters")
-           }
-        }else{
-            showEmailwarning("your email is invalid")
+let email, password;
+$(document).ready(function () {
+  // alert("hellow ")
+  loginBtn.click(function () {
+    setVariables();
+    if (checkEmailValidation(email) == true) {
+      if (checkPasswordValidation(password) == true) {
+        cleanUpWarning();
+        let data = {
+          email,
+          password,
         };
-    })
-
-
-})
-
-function setVariables(){
-    email = emailT.val();
-    password = passwordT.val();
-}
-
-function checkEmailValidation(email){
-    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
-    return true;
-        }
-    return false;
-}
-
-function checkPasswordValidation(password){
-    if(password.length >= 8 ){
-        return true
+        callBackEnd(data);
+      } else {
+        showPasswordwarning("your password should be 8 chracters");
+        cleanUpPassword();
+      }
+    } else {
+      showEmailwarning("your email is invalid");
     }
-    return false ; 
+  });
+});
+
+function setVariables() {
+  email = emailT.val();
+  password = passwordT.val();
 }
 
+function checkEmailValidation(email) {
+  if (
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email
+    )
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function checkPasswordValidation(password) {
+  if (password.length >= 8) {
+    return true;
+  }
+  return false;
+}
 
 function showEmailwarning(text) {
-$("#email-warning").html(text);
-$("#email-parent").addClass("show-valid");
+  $("#email-warning").html(text);
+  $("#email-parent").addClass("show-valid");
 }
 
 function showPasswordwarning(text) {
- $("#password-warning").html(text);
- $("#password-parent").addClass("show-valid");
+  $("#password-warning").html(text);
+  $("#password-parent").addClass("show-valid");
 }
 
-function  cleanUpWarning(){
- $("#password-parent").removeClass("show-valid");
- $("#email-parent").removeClass("show-valid");
+function cleanUpWarning() {
+  $("#password-parent").removeClass("show-valid");
+  $("#email-parent").removeClass("show-valid");
 }
-
 
 function callBackEnd(data) {
-$.ajax({
-url: "backend/php/login.php",
-type: "POST",
-dataType: "json",
-data: data,
-success: function (data) {
-manipulateData(data);
-},
-});
+  $.ajax({
+    url: "backend/php/login.php",
+    type: "POST",
+    dataType: "json",
+    data: data,
+    success: function (data) {
+      manipulateData(data);
+    },
+  });
 }
-
+function cleanUpPassword(){
+    passwordT.val("");
+}
 
 function manipulateData(data) {
-if (data.length > 0) {
-window.location.href = "http://localhost/ad/site/index.html";
-} else {
-showPasswordwarning("Your password is incorrect ! ");
-}
+  if (data.length > 0) {
+    window.location.href = "http://localhost/ad/site/index.html";
+  } else {
+    showPasswordwarning("Your password is incorrect ! ");
+    cleanUpPassword();
+  }
 }
 
+module.exports = checkEmailValidation ; 
